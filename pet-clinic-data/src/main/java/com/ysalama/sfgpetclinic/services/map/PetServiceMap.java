e04edd79b4940cb7fa7dfce9cf758de889493bb1
Youssef Salama <youssefhanii@hotmail.com>
2019-08-20 01:52:17 +0200

    Implement Map Based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/ysalama/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.ysalama.sfgpetclinic.services.map;
+
+import com.ysalama.sfgpetclinic.model.Pet;
+import com.ysalama.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
