531f5dc1a9de87d9b59ace1db4df67ed4d8549ae
Youssef Salama <youssefhanii@hotmail.com>
2019-08-20 02:37:10 +0200

    Adding Index Page and controller plus fixed project setup. Closes #8.

--- pet-clinic-web/src/main/java/com/ysalama/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/ysalama/sfgpetclinic/SfgPetClinicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgPetClinicApplication.class, args);
+        System.out.println("hi i started");SpringApplication.run(SfgPetClinicApplication.class, args);
     }
 
 }
