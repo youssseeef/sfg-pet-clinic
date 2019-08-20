ed35e6e907d988e7ac235dde1a388d6ea25b0baa
Youssef Salama <youssefhanii@hotmail.com>
2019-08-20 03:21:08 +0200

    Added owners page and controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/com/ysalama/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.ysalama.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(){
+        return "owners/index";
+    }
+}
